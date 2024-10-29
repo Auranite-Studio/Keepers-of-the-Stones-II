9e167546e71ab780a84d032a8d6f8501b0de7d46
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-10-29 20:57:28 +0300

    More fixes

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -1,4 +1,6 @@
 What's new in Keepers of the Stones II version 1.0.0 Release Candidate 1?<br/>
 <br />- [Bugfix] Fixed a bug with recharging the darkness stone
 <br />- [Bugfix] Fixed a bug that caused the eternal power lock effect
+<br />- [Bugfix] Fixed the range of melee abilities
 <br />- [Bugfix] Fixed a bug where power transfer spent star points on active elemental masters
+<br />- [Bugfix] Fixed a bug where mushrooms were not placed freely by mushroom masters
--- src/main/java/com/esmods/keepersofthestonestwo/procedures/MushroomsSpecialAttackProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/MushroomsSpecialAttackProcedure.java
@@ -127,20 +127,32 @@ public class MushroomsSpecialAttackProcedure {
 										.getZ()));
 						List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(1.3 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
 						for (Entity entityiterator : _entfound) {
-							if (!(entityiterator == entity)) {
-								if (world.getBlockState(BlockPos.containing(x, y - 1, z)).canOcclude()) {
-									world.setBlock(BlockPos.containing(entityiterator.getX(), entityiterator.getY(), entityiterator.getZ()), PowerModBlocks.PURPLE_MUSHROOM.get().defaultBlockState(), 3);
-									world.levelEvent(2001, BlockPos.containing(entityiterator.getX(), entityiterator.getY(), entityiterator.getZ()), Block.getId(PowerModBlocks.PURPLE_MUSHROOM.get().defaultBlockState()));
-									entityiterator.hurt(new DamageSource(world.holderOrThrow(ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.parse("power:elemental_powers"))), entity), (float) 13.5);
-									if (world instanceof Level _level) {
-										if (!_level.isClientSide()) {
-											_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.fungus.break")), SoundSource.PLAYERS, 1, 1);
-										} else {
-											_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.fungus.break")), SoundSource.PLAYERS, 1, 1, false);
-										}
+							if (world.getBlockState(BlockPos.containing(x, y - 1, z)).canOcclude()) {
+								world.setBlock(new BlockPos(
+										entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(Scaling)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, entity)).getBlockPos()
+												.getX(),
+										entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(Scaling)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, entity)).getBlockPos()
+												.getY(),
+										entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(Scaling)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, entity)).getBlockPos()
+												.getZ()),
+										PowerModBlocks.PURPLE_MUSHROOM.get().defaultBlockState(), 3);
+								world.levelEvent(2001,
+										new BlockPos(
+												entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(Scaling)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, entity))
+														.getBlockPos().getX(),
+												entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(Scaling)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, entity))
+														.getBlockPos().getY(),
+												entity.level().clip(new ClipContext(entity.getEyePosition(1f), entity.getEyePosition(1f).add(entity.getViewVector(1f).scale(Scaling)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, entity))
+														.getBlockPos().getZ()),
+										Block.getId(PowerModBlocks.PURPLE_MUSHROOM.get().defaultBlockState()));
+								if (world instanceof Level _level) {
+									if (!_level.isClientSide()) {
+										_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.fungus.break")), SoundSource.PLAYERS, 1, 1);
+									} else {
+										_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.fungus.break")), SoundSource.PLAYERS, 1, 1, false);
 									}
-									success = true;
 								}
+								success = true;
 							}
 						}
 					}
