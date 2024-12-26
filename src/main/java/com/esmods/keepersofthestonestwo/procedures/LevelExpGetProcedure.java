7234a20fef67b2aa3763d2baf46c0f5290b68c2c
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-12-26 19:50:09 +0300

    Some fixes lvl exp

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/LevelExpGetProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/LevelExpGetProcedure.java
@@ -5,6 +5,9 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.Event;
 
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.tags.TagKey;
@@ -14,6 +17,9 @@ import net.minecraft.core.registries.Registries;
 
 import javax.annotation.Nullable;
 
+import java.util.List;
+import java.util.Comparator;
+
 import com.esmods.keepersofthestonestwo.network.PowerModVariables;
 import com.esmods.keepersofthestonestwo.configuration.PowerConfigConfiguration;
 
@@ -22,15 +28,15 @@ public class LevelExpGetProcedure {
 	@SubscribeEvent
 	public static void onEntityDeath(LivingDeathEvent event) {
 		if (event.getEntity() != null) {
-			execute(event, event.getEntity(), event.getSource().getEntity());
+			execute(event, event.getEntity().level(), event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), event.getEntity(), event.getSource().getEntity());
 		}
 	}
 
-	public static void execute(Entity entity, Entity sourceentity) {
-		execute(null, entity, sourceentity);
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
+		execute(null, world, x, y, z, entity, sourceentity);
 	}
 
-	private static void execute(@Nullable Event event, Entity entity, Entity sourceentity) {
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
 		if (entity == null || sourceentity == null)
 			return;
 		if (PowerConfigConfiguration.ENABLE_LEVELS.get() == true) {
@@ -61,9 +67,17 @@ public class LevelExpGetProcedure {
 					}
 				} else if (entity.getType().is(TagKey.create(Registries.ENTITY_TYPE, ResourceLocation.parse("power:xp/50")))) {
 					{
-						PowerModVariables.PlayerVariables _vars = sourceentity.getData(PowerModVariables.PLAYER_VARIABLES);
-						_vars.level_exp = sourceentity.getData(PowerModVariables.PLAYER_VARIABLES).level_exp + 50;
-						_vars.syncPlayerVariables(sourceentity);
+						final Vec3 _center = new Vec3(x, y, z);
+						List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(64 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
+						for (Entity entityiterator : _entfound) {
+							if (entityiterator instanceof Player || entityiterator instanceof ServerPlayer) {
+								{
+									PowerModVariables.PlayerVariables _vars = entityiterator.getData(PowerModVariables.PLAYER_VARIABLES);
+									_vars.level_exp = entityiterator.getData(PowerModVariables.PLAYER_VARIABLES).level_exp + 50;
+									_vars.syncPlayerVariables(entityiterator);
+								}
+							}
+						}
 					}
 				} else if (entity instanceof Player || entity instanceof ServerPlayer) {
 					if (entity.getData(PowerModVariables.PLAYER_VARIABLES).active_power) {
