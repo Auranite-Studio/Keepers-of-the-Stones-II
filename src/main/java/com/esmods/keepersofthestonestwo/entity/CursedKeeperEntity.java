936e47640ce6436dc1584c2c374550c30094c110
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2025-01-06 00:33:54 +0300

    Increased the Cursed Keeper's health to 700

--- src/main/java/com/esmods/keepersofthestonestwo/entity/CursedKeeperEntity.java
+++ src/main/java/com/esmods/keepersofthestonestwo/entity/CursedKeeperEntity.java
@@ -248,7 +248,7 @@ public class CursedKeeperEntity extends Monster {
 	public static AttributeSupplier.Builder createAttributes() {
 		AttributeSupplier.Builder builder = Mob.createMobAttributes();
 		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.25);
-		builder = builder.add(Attributes.MAX_HEALTH, 350);
+		builder = builder.add(Attributes.MAX_HEALTH, 700);
 		builder = builder.add(Attributes.ARMOR, 10);
 		builder = builder.add(Attributes.ATTACK_DAMAGE, 100);
 		builder = builder.add(Attributes.FOLLOW_RANGE, 128);
