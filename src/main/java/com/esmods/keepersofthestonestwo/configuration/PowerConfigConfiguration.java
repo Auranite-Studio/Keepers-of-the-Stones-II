254e056ec4d0491bc3eec29fdcc3bedd5ae4d1df
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2025-02-02 00:32:04 +0300

    Some fixes

--- src/main/java/com/esmods/keepersofthestonestwo/configuration/PowerConfigConfiguration.java
+++ src/main/java/com/esmods/keepersofthestonestwo/configuration/PowerConfigConfiguration.java
@@ -12,7 +12,7 @@ public class PowerConfigConfiguration {
 	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_LEVELS;
 	static {
 		BUILDER.push("stone_distribution_settings");
-		FIRST_JOIN_STONES_DISTRIBUTION = BUILDER.comment("Allows players to select a stone when entering the world").define("first_join_stones_distribution", true);
+		FIRST_JOIN_STONES_DISTRIBUTION = BUILDER.comment("Allows players to select a stone when entering the world").define("first_join_stones_distribution", false);
 		ONE_PLAYER_LIMIT_STONES = BUILDER.comment("Enables the One Player, one Power mode").define("one_player_limit_stones", true);
 		LIMIT_NUMBER_STONES = BUILDER.comment("Limits the number of stones to receive and includes their uniqueness").define("limit_number_stones", true);
 		BUILDER.pop();
