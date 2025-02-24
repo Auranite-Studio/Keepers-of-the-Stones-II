0a90cd915ad8c10d448b76d3bf131f8c778c8f9d
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2025-02-24 23:53:15 +0300

    Return pehkui

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/TimeStoppedPriIstiechieniiEffiektaProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/TimeStoppedPriIstiechieniiEffiektaProcedure.java
@@ -1,5 +1,8 @@
 package com.esmods.keepersofthestonestwo.procedures;
 
+import virtuoel.pehkui.api.ScaleTypes;
+import virtuoel.pehkui.api.ScaleOperations;
+
 import net.minecraft.world.entity.Entity;
 
 import com.esmods.keepersofthestonestwo.network.PowerModVariables;
@@ -8,6 +11,7 @@ public class TimeStoppedPriIstiechieniiEffiektaProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
+		ScaleTypes.MOTION.getScaleData(entity).setTargetScale((float) ScaleOperations.SET.applyAsDouble(ScaleTypes.MOTION.getScaleData(entity).getTargetScale(), 1));
 		{
 			boolean _setval = false;
 			entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
