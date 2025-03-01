1b76739e43e4352cfd20250057e7f2bc1b136747
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2025-03-02 02:04:53 +0300

    Inital return animations

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/DetransformAnimationProcedure.java
+++
@@ -1,6 +0,0 @@
-package com.esmods.keepersofthestonestwo.procedures;
-
-public class DetransformAnimationProcedure {
-	public static void execute() {
-	}
-}
--- src/main/java/com/esmods/keepersofthestonestwo/procedures/TechnologyStoneUseProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/TechnologyStoneUseProcedure.java
@@ -43,7 +43,7 @@ public class TechnologyStoneUseProcedure {
 					_vars.power = entity.getData(PowerModVariables.PLAYER_VARIABLES).max_power;
 					_vars.syncPlayerVariables(entity);
 				}
-				SendClientPackageActivationStoneVFXProcedure.execute(world, x, y, z);
+				SendClientPackageActivationStoneVFXProcedure.execute(world, x, y, z, entity);
 			}
 		}
 	}
