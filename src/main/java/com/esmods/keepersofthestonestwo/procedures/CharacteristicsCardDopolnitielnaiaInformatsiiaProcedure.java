f757716fb4b54223548da57cb4cae35b9d35e3bc
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2025-03-22 13:35:51 +0300

    Attempt resolve automate conflicts

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/CharacteristicsCardDopolnitielnaiaInformatsiiaProcedure.java
+++
@@ -1,9 +0,0 @@
-package com.esmods.keepersofthestonestwo.procedures;
-
-import net.minecraft.world.item.ItemStack;
-
-public class CharacteristicsCardDopolnitielnaiaInformatsiiaProcedure {
-	public static String execute(ItemStack itemstack) {
-		return !(itemstack.getOrCreateTag().getString("name")).equals("") ? "\u00A77" + itemstack.getOrCreateTag().getString("name") : "\u00A77Null";
-	}
-}
