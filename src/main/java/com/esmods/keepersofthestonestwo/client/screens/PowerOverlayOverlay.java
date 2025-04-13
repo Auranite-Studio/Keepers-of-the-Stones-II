b9bd6fa19f84703207e8716dc92832228cf7cb04
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2025-04-13 14:21:24 +0300

    Some works for runes

--- src/main/java/com/esmods/keepersofthestonestwo/client/screens/PowerOverlayOverlay.java
+++ src/main/java/com/esmods/keepersofthestonestwo/client/screens/PowerOverlayOverlay.java
@@ -20,9 +20,12 @@ import com.mojang.blaze3d.platform.GlStateManager;
 
 import com.esmods.keepersofthestonestwo.procedures.GetStarPointsProcedure;
 import com.esmods.keepersofthestonestwo.procedures.GetActiveProcedure;
+import com.esmods.keepersofthestonestwo.procedures.BlueRune3CheckProcedure;
+import com.esmods.keepersofthestonestwo.procedures.BlueRune2CheckProcedure;
+import com.esmods.keepersofthestonestwo.procedures.BlueRune1CheckProcedure;
 
 @EventBusSubscriber({Dist.CLIENT})
-public class StarPointsOverlayOverlay {
+public class PowerOverlayOverlay {
 	@SubscribeEvent(priority = EventPriority.NORMAL)
 	public static void eventHandler(RenderGuiEvent.Pre event) {
 		int w = event.getGuiGraphics().guiWidth();
@@ -47,6 +50,17 @@ public class StarPointsOverlayOverlay {
 		if (GetActiveProcedure.execute(entity)) {
 			event.getGuiGraphics().blit(ResourceLocation.parse("power:textures/screens/star_points_overlay.png"), 2, 28, 0, 0, 59, 20, 59, 20);
 
+			event.getGuiGraphics().blit(ResourceLocation.parse("power:textures/screens/runes_inventory.png"), w - 77, h - 40, 0, 0, 68, 33, 68, 33);
+
+			if (BlueRune1CheckProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("power:textures/screens/star_extra_booster_1.png"), w - 69, h - 32, 0, 0, 16, 16, 16, 16);
+			}
+			if (BlueRune2CheckProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("power:textures/screens/star_extra_booster_2.png"), w - 69, h - 32, 0, 0, 16, 16, 16, 16);
+			}
+			if (BlueRune3CheckProcedure.execute(entity)) {
+				event.getGuiGraphics().blit(ResourceLocation.parse("power:textures/screens/star_extra_booster_3.png"), w - 69, h - 32, 0, 0, 16, 16, 16, 16);
+			}
 			event.getGuiGraphics().drawString(Minecraft.getInstance().font,
 
 					GetStarPointsProcedure.execute(entity), 31, 35, -1, false);
