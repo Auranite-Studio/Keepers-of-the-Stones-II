116ed523599a6dcb4257c195168cd34a9b3fe912
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2025-04-14 00:43:17 +0300

    Inital port

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/DetransformAnimationStartProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/DetransformAnimationStartProcedure.java
@@ -1,15 +1,11 @@
 package com.esmods.keepersofthestonestwo.procedures;
 
-import net.neoforged.neoforge.network.PacketDistributor;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.Event;
 
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.server.level.ServerLevel;
 
 import javax.annotation.Nullable;
 
@@ -19,24 +15,17 @@ import com.esmods.keepersofthestonestwo.network.PowerModVariables;
 public class DetransformAnimationStartProcedure {
 	@SubscribeEvent
 	public static void onPlayerTick(PlayerTickEvent.Post event) {
-		execute(event, event.getEntity().level(), event.getEntity());
+		execute(event, event.getEntity());
 	}
 
-	public static void execute(LevelAccessor world, Entity entity) {
-		execute(null, world, entity);
+	public static void execute(Entity entity) {
+		execute(null, entity);
 	}
 
-	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity) {
+	private static void execute(@Nullable Event event, Entity entity) {
 		if (entity == null)
 			return;
 		if (entity.getData(PowerModVariables.PLAYER_VARIABLES).detransform_anim_trigger) {
-			if (world.isClientSide()) {
-				AnimationsModuleSetupProcedure.setAnimationClientside((Player) entity, "animation.player.detransformation", false);
-			}
-			if (!world.isClientSide()) {
-				if (entity instanceof Player)
-					PacketDistributor.sendToPlayersInDimension((ServerLevel) entity.level(), new AnimationsModuleSetupProcedure.PowerModAnimationMessage("animation.player.detransformation", entity.getId(), false));
-			}
 			{
 				PowerModVariables.PlayerVariables _vars = entity.getData(PowerModVariables.PLAYER_VARIABLES);
 				_vars.detransform_anim_trigger = false;
--- src/main/java/com/esmods/keepersofthestonestwo/procedures/SendClientPackageActivationStoneVFXProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/SendClientPackageActivationStoneVFXProcedure.java
@@ -1,13 +1,10 @@
 package com.esmods.keepersofthestonestwo.procedures;
 
-import net.neoforged.neoforge.network.PacketDistributor;
-
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.BlockPos;
@@ -16,19 +13,14 @@ public class SendClientPackageActivationStoneVFXProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
-		if (world instanceof Level _level) {
-			if (!_level.isClientSide()) {
-				_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("power:stone_activation")), SoundSource.PLAYERS, 1, 1);
-			} else {
-				_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("power:stone_activation")), SoundSource.PLAYERS, 1, 1, false);
+		if (entity instanceof Player) {
+			if (world instanceof Level _level) {
+				if (!_level.isClientSide()) {
+					_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("power:stone_activation")), SoundSource.PLAYERS, 1, 1);
+				} else {
+					_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("power:stone_activation")), SoundSource.PLAYERS, 1, 1, false);
+				}
 			}
-		}
-		if (world.isClientSide()) {
-			AnimationsModuleSetupProcedure.setAnimationClientside((Player) entity, "animation.player.transformation", false);
-		}
-		if (!world.isClientSide()) {
-			if (entity instanceof Player)
-				PacketDistributor.sendToPlayersInDimension((ServerLevel) entity.level(), new AnimationsModuleSetupProcedure.PowerModAnimationMessage("animation.player.transformation", entity.getId(), false));
 		}
 	}
 }
