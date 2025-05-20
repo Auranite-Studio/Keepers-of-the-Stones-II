24c0390fc1d3e9d1464190c079db7f8203153569
ShadowMoon07 <209759786+ShadowMoon07@users.noreply.github.com>
2025-05-20 04:12:09 +0300

    Green runes is done and some changes

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -1,9 +1,11 @@
 
 **New features**
 - Added new elements - heat and shockwave
+- Added five green runes
 
 **Improvemnts** 
 - The keeper's box can now hold more than 48 elements
+- The rune panel will now hide if there are no active runes
 
 **API changes**
 - Colorful Powers API has been updated to 26
--- src/main/java/com/esmods/keepersofthestonestwo/procedures/RuneAbilitiesUsingProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/RuneAbilitiesUsingProcedure.java
@@ -1,17 +1,30 @@
 package com.esmods.keepersofthestonestwo.procedures;
 
+import org.joml.Vector3f;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.Mth;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.particles.DustParticleOptions;
 
 import com.esmods.keepersofthestonestwo.network.PowerModVariables;
 import com.esmods.keepersofthestonestwo.init.PowerModMobEffects;
 import com.esmods.keepersofthestonestwo.init.PowerModItems;
 
 public class RuneAbilitiesUsingProcedure {
-	public static void execute(Entity entity) {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
+		double particleRadius = 0;
+		double particleAmount = 0;
 		if (entity.getData(PowerModVariables.PLAYER_VARIABLES).green_rune_slot.getItem() == PowerModItems.PROTECTION_RUNE.get()) {
 			if (entity.getData(PowerModVariables.PLAYER_VARIABLES).power >= 90) {
 				if (!(entity instanceof LivingEntity _livEnt1 && _livEnt1.hasEffect(PowerModMobEffects.TECHNOBARRIER))) {
@@ -48,6 +61,116 @@ public class RuneAbilitiesUsingProcedure {
 						_vars.power = entity.getData(PowerModVariables.PLAYER_VARIABLES).power - 90;
 						_vars.syncPlayerVariables(entity);
 					}
+				}
+			}
+		}
+		if (entity.getData(PowerModVariables.PLAYER_VARIABLES).green_rune_slot.getItem() == PowerModItems.INVISIBILITY_RUNE.get()) {
+			if (entity.getData(PowerModVariables.PLAYER_VARIABLES).power >= 90) {
+				if (entity instanceof Player && entity.getData(PowerModVariables.PLAYER_VARIABLES).ability_using == false) {
+					particleAmount = 8;
+					particleRadius = 2;
+					for (int index0 = 0; index0 < 60; index0++) {
+						for (int index1 = 0; index1 < (int) particleAmount; index1++) {
+							if (world instanceof ServerLevel)
+								((ServerLevel) world).sendParticles((new DustParticleOptions(new Vector3f(0 / 255.0F, 255 / 255.0F, 0 / 255.0F), 1)), (x + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius),
+										(y + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius), (z + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius), 1, (Mth.nextDouble(RandomSource.create(), -0.001, 0.001)),
+										(Mth.nextDouble(RandomSource.create(), -0.001, 0.001)), (Mth.nextDouble(RandomSource.create(), -0.001, 0.001)), 1);
+						}
+					}
+					{
+						PowerModVariables.PlayerVariables _vars = entity.getData(PowerModVariables.PLAYER_VARIABLES);
+						_vars.helmet = (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.HEAD) : ItemStack.EMPTY).copy();
+						_vars.syncPlayerVariables(entity);
+					}
+					{
+						PowerModVariables.PlayerVariables _vars = entity.getData(PowerModVariables.PLAYER_VARIABLES);
+						_vars.chestplate = (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.CHEST) : ItemStack.EMPTY).copy();
+						_vars.syncPlayerVariables(entity);
+					}
+					{
+						PowerModVariables.PlayerVariables _vars = entity.getData(PowerModVariables.PLAYER_VARIABLES);
+						_vars.leggings = (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.LEGS) : ItemStack.EMPTY).copy();
+						_vars.syncPlayerVariables(entity);
+					}
+					{
+						PowerModVariables.PlayerVariables _vars = entity.getData(PowerModVariables.PLAYER_VARIABLES);
+						_vars.boots = (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).copy();
+						_vars.syncPlayerVariables(entity);
+					}
+					{
+						Entity _entity = entity;
+						if (_entity instanceof Player _player) {
+							_player.getInventory().armor.set(3, ItemStack.EMPTY);
+							_player.getInventory().setChanged();
+						} else if (_entity instanceof LivingEntity _living) {
+							_living.setItemSlot(EquipmentSlot.HEAD, ItemStack.EMPTY);
+						}
+					}
+					{
+						Entity _entity = entity;
+						if (_entity instanceof Player _player) {
+							_player.getInventory().armor.set(2, ItemStack.EMPTY);
+							_player.getInventory().setChanged();
+						} else if (_entity instanceof LivingEntity _living) {
+							_living.setItemSlot(EquipmentSlot.CHEST, ItemStack.EMPTY);
+						}
+					}
+					{
+						Entity _entity = entity;
+						if (_entity instanceof Player _player) {
+							_player.getInventory().armor.set(1, ItemStack.EMPTY);
+							_player.getInventory().setChanged();
+						} else if (_entity instanceof LivingEntity _living) {
+							_living.setItemSlot(EquipmentSlot.LEGS, ItemStack.EMPTY);
+						}
+					}
+					{
+						Entity _entity = entity;
+						if (_entity instanceof Player _player) {
+							_player.getInventory().armor.set(0, ItemStack.EMPTY);
+							_player.getInventory().setChanged();
+						} else if (_entity instanceof LivingEntity _living) {
+							_living.setItemSlot(EquipmentSlot.FEET, ItemStack.EMPTY);
+						}
+					}
+				}
+				if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+					_entity.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY, 600, 3, false, false));
+				{
+					PowerModVariables.PlayerVariables _vars = entity.getData(PowerModVariables.PLAYER_VARIABLES);
+					_vars.abilities_timer = 600;
+					_vars.syncPlayerVariables(entity);
+				}
+				{
+					PowerModVariables.PlayerVariables _vars = entity.getData(PowerModVariables.PLAYER_VARIABLES);
+					_vars.ability_using = true;
+					_vars.syncPlayerVariables(entity);
+				}
+				{
+					PowerModVariables.PlayerVariables _vars = entity.getData(PowerModVariables.PLAYER_VARIABLES);
+					_vars.power = entity.getData(PowerModVariables.PLAYER_VARIABLES).power - 90;
+					_vars.syncPlayerVariables(entity);
+				}
+			}
+		}
+		if (entity.getData(PowerModVariables.PLAYER_VARIABLES).green_rune_slot.getItem() == PowerModItems.HEALING_RUNE.get()) {
+			if (entity.getData(PowerModVariables.PLAYER_VARIABLES).power >= 90) {
+				particleAmount = 8;
+				particleRadius = 2;
+				for (int index2 = 0; index2 < 60; index2++) {
+					for (int index3 = 0; index3 < (int) particleAmount; index3++) {
+						if (world instanceof ServerLevel)
+							((ServerLevel) world).sendParticles((new DustParticleOptions(new Vector3f(0 / 255.0F, 255 / 255.0F, 0 / 255.0F), 1)), (x + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius),
+									(y + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius), (z + 0 + Mth.nextDouble(RandomSource.create(), -1, 1) * particleRadius), 1, (Mth.nextDouble(RandomSource.create(), -0.001, 0.001)),
+									(Mth.nextDouble(RandomSource.create(), -0.001, 0.001)), (Mth.nextDouble(RandomSource.create(), -0.001, 0.001)), 1);
+					}
+				}
+				if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+					_entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 200, 4, false, false));
+				{
+					PowerModVariables.PlayerVariables _vars = entity.getData(PowerModVariables.PLAYER_VARIABLES);
+					_vars.power = entity.getData(PowerModVariables.PLAYER_VARIABLES).power - 90;
+					_vars.syncPlayerVariables(entity);
 				}
 			}
 		}
