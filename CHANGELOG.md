62598c276efb9c89218655dd32318cc932bcd043
ShadowMoon07 <209759786+ShadowMoon07@users.noreply.github.com>
2025-08-19 17:25:04 +0300

    Fixed a bug that caused the sand master's armor to not fully disappear when the stone was deactivated

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,7 +8,7 @@
 - *N/A*
 
 **Bugfixes**
-- [Bugfix] 
+- [Bugfix] Fixed a bug that caused the sand master's armor to not fully disappear when the stone was deactivated
 
 **Known issues**
 - *N/A*
--- src/main/java/com/esmods/keepersofthestonestwo/item/SandArmorItem.java
+++ src/main/java/com/esmods/keepersofthestonestwo/item/SandArmorItem.java
@@ -24,7 +24,6 @@ import java.util.EnumMap;
 
 import com.google.common.collect.Iterables;
 
-import com.esmods.keepersofthestonestwo.procedures.ReturnStoneAfterDeadProcedure;
 import com.esmods.keepersofthestonestwo.procedures.RemoveForbiddenItemProcedure;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
@@ -73,7 +72,7 @@ public abstract class SandArmorItem extends ArmorItem {
 		public void inventoryTick(ItemStack itemstack, Level world, Entity entity, int slot, boolean selected) {
 			super.inventoryTick(itemstack, world, entity, slot, selected);
 			if (entity instanceof Player player && Iterables.contains(player.getArmorSlots(), itemstack)) {
-				ReturnStoneAfterDeadProcedure.execute(entity);
+				RemoveForbiddenItemProcedure.execute(world, entity, itemstack);
 			}
 		}
 	}
