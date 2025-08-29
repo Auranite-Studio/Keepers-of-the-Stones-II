5be1f6969683e0101e895a642ab85100687c930f
ShadowMoon07 <209759786+ShadowMoon07@users.noreply.github.com>
2025-08-30 00:46:46 +0300

    Added sound event when rune crafted

--- src/main/java/com/esmods/keepersofthestonestwo/procedures/RemoveWhenRuneIsCuttedProcedure.java
+++ src/main/java/com/esmods/keepersofthestonestwo/procedures/RemoveWhenRuneIsCuttedProcedure.java
@@ -9,6 +9,9 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.BlockPos;
 
 public class RemoveWhenRuneIsCuttedProcedure {
@@ -24,6 +27,13 @@ public class RemoveWhenRuneIsCuttedProcedure {
 			ItemStack _stk = _itemHandlerModifiable.getStackInSlot(_slotid).copy();
 			_stk.shrink(1);
 			_itemHandlerModifiable.setStackInSlot(_slotid, _stk);
+		}
+		if (world instanceof Level _level) {
+			if (!_level.isClientSide()) {
+				_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("ui.stonecutter.take_result")), SoundSource.NEUTRAL, 1, 1);
+			} else {
+				_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("ui.stonecutter.take_result")), SoundSource.NEUTRAL, 1, 1, false);
+			}
 		}
 		if (!world.isClientSide()) {
 			BlockPos _bp = BlockPos.containing(x, y, z);
