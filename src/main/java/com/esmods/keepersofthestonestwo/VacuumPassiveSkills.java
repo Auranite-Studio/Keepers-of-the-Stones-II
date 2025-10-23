dee5fa1ca6c1a8e2ef27bf3d3393ea2f830bb53a
ShadowMoon07 <209759786+ShadowMoon07@users.noreply.github.com>
2025-10-24 00:51:40 +0300

    Fix

--- src/main/java/com/esmods/keepersofthestonestwo/VacuumPassiveSkills.java
+++ src/main/java/com/esmods/keepersofthestonestwo/VacuumPassiveSkills.java
@@ -1,4 +1,4 @@
-package your.modid.common.event;
+package com.esmods.keepersofthestonestwo;
 
 import com.esmods.keepersofthestonestwo.init.PowerModMobEffects;
 import net.minecraft.server.level.ServerPlayer;
