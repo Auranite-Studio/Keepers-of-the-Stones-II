38d938179943dd46b0ba0bbdab96f44753f62d68
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2025-11-12 06:33:19 +0300

    More works

--- BETA_CHANGELOG.md
+++ BETA_CHANGELOG.md
@@ -1,14 +1,15 @@
 **New features**
-* *N/A*
+* The cave biome "Deposit of Energium" has been added to the Сursed World
 
 **Improvemnts**
 * The fill rate of the Eternal Curse meter now depends on the effect level
 * Starting now, each effect level increases the fill speed by 25% per level
 * The Eternal Curse effect no longer slows the player down
 * Increased protection for bosses and mobs in the cursed world
+* The cursed world now consists of a cursed stone instead of an ordinary one.
 
 **API changes**
-* *N/A*
+* Added the 'tp' argument for the 'pw' command, for teleportation to mod dimensions
 
 **Bugfixes**
 * \[Bugfix] 
--- src/main/java/com/esmods/keepersofthestonestwo/command/PwCommand.java
+++ src/main/java/com/esmods/keepersofthestonestwo/command/PwCommand.java
@@ -181,7 +181,21 @@ public class PwCommand {
 
 					QuakePowerProcedure.execute(world, x, y, z, arguments);
 					return 0;
-				}))))));
+				}))))).then(Commands.literal("tp").then(Commands.literal("cursed_world").executes(arguments -> {
+					Level world = arguments.getSource().getUnsidedLevel();
+					double x = arguments.getSource().getPosition().x();
+					double y = arguments.getSource().getPosition().y();
+					double z = arguments.getSource().getPosition().z();
+					Entity entity = arguments.getSource().getEntity();
+					if (entity == null && world instanceof ServerLevel _servLevel)
+						entity = FakePlayerFactory.getMinecraft(_servLevel);
+					Direction direction = Direction.DOWN;
+					if (entity != null)
+						direction = entity.getDirection();
+
+					TpToCursedWorldProcedure.execute(arguments, entity);
+					return 0;
+				}))));
 	}
 
 }
